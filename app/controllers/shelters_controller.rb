8d131204a6ec1edec32d69f5c6ab37fe883d86c6
Sean Steel <seanrsteel@gmail.com>
2020-10-06 14:48:05 -0600

    Add shelters new file and set up routing

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -3,7 +3,13 @@ class SheltersController < ApplicationController
     @shelters = Shelter.all
   end
 
+  def new
+  end
+
   def show
     @shelter = Shelter.find(params[:id])
   end
+
+  def create
+  end 
 end
