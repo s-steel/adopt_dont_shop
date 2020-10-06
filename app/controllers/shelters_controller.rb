f8e746d2bf0640eca363cad340da45f16b3f51d7
Sean Steel <seanrsteel@gmail.com>
2020-10-06 15:44:16 -0600

    Create edit method and route

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -23,4 +23,8 @@ class SheltersController < ApplicationController
 
     redirect_to '/shelters'
   end
+
+  def edit
+    @shelter = Shelter.find(params[:id])
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get '/shelters/new', to: 'shelters#new'
   post '/shelters', to: 'shelters#create'
   get '/shelters/:id', to: 'shelters#show'
+  get '/shelters/:id/edit', to: 'shelters#edit'
 end
