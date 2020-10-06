07e5e09ff7b7014c78fe1c2e290070c2ed22c5d4
Sean Steel <seanrsteel@gmail.com>
2020-10-06 08:32:11 -0600

    Add shelter class and shelters_controllers class

---
+++ app/controllers/shelters_controller.rb
@@ -0,0 +1,5 @@
+class SheltersController < ApplicationController
+  def index
+     
+  end
+end
---
+++ spec/features/shelters/see_shelters_spec.rb
@@ -0,0 +1,22 @@
+# require 'rails_helper'
+#
+# Rspec.describe "shelter index page", type: :feature do
+#   it "can see shelter info" do
+#     shelter_1 = Shelter.create(name:
+#                                address:
+#                                city:
+#                                state:
+#                                zip:)
+#     shelter_2 = Shelter.create(name:
+#                                address:
+#                                city:
+#                                state:
+#                                zip:)
+#     visit "/shelter"
+#
+#     expect(page).to have_content(shelter_1.name)
+#     expect(page).to have_content("Zip: #{shelter_1.zip}")
+#     expect(page).to have_content(shelter_2.name)
+#     expect(page).to have_content("Zip: #{shelter_2.zip}")
+#   end
+# end
