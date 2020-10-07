5a58e4d2b89f3e65006831fdbfd8b9abe0185a9e
Sean Steel <seanrsteel@gmail.com>
2020-10-07 16:39:10 -0600

    Add pet and pet_controller class

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'home#index'
   get '/shelters', to: 'shelters#index'
+  get '/pets', to: 'pets#index'
   get '/shelters/new', to: 'shelters#new'
   post '/shelters', to: 'shelters#create'
   get '/shelters/:id', to: 'shelters#show'
