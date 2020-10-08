9ea620497a341771f1934fa859a9f446e859f747
Sean Steel <seanrsteel@gmail.com>
2020-10-08 15:27:52 -0600

    Add pet_spec tests to validate presence

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,10 @@
 class Pet < ApplicationRecord
   belongs_to :shelter
+
+  validates_presence_of :image
+  validates_presence_of :name
+  validates_presence_of :approximate_age
+  validates_presence_of :sex
+  validates_presence_of :description
+  validates_presence_of :adoption_status
 end
