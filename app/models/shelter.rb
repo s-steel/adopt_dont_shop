5afaef1ee9124cc693f7edadb9634bd6531a2747
Sean Steel <seanrsteel@gmail.com>
2020-10-08 15:34:41 -0600

    Create shelter_spec tests for should and validate_presence

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,4 +1,9 @@
 class Shelter < ApplicationRecord
   has_many :pets
 
+  validates_presence_of :name
+  validates_presence_of :address
+  validates_presence_of :city
+  validates_presence_of :state
+  validates_presence_of :zip
 end
