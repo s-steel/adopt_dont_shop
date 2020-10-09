bed5bb37c9e0ae617a6a165a72002e9d8316cf5c
Sean Steel <seanrsteel@gmail.com>
2020-10-08 20:14:11 -0600

    Finish feature tests for shelter create

--- spec/features/shelters/create_spec.rb
+++ spec/features/shelters/create_spec.rb
@@ -4,23 +4,23 @@ describe "As a visitor" do
   describe "When I visit the Shelter Index page and click 'New Shelter' link" do
     it "I can go to new page" do
       visit '/shelters'
-      expect(page).to have_link('New Shelter')
-      click_link('New Shelter')
-      expect(current_path)to eq('/shelters/new')
-      fill_in 'name', with: 'Test Shelter 1'
-      fill_in 'address', with: '456 South St'
-      fill_in 'city', with: 'Springfield'
-      fill_in 'state', with: 'IL'
-      fill_in 'zip', with: '85308'
+      expect(page).to have_button('Add A New Shelter')
+      click_button('Add A New Shelter')
+      expect(current_path).to eq('/shelters/new')
+      fill_in 'shelter[name]', with: 'Test Shelter 1'
+      fill_in 'shelter[address]', with: '456 South St'
+      fill_in 'shelter[city]', with: 'Springfield'
+      fill_in 'shelter[state]', with: 'IL'
+      fill_in 'shelter[zip]', with: 85308
 
       click_button('Create Shelter')
-      expect(current_path)to eq('/shelters')
+      expect(current_path).to eq('/shelters')
+      expect(page).to have_content("Test Shelter 1")
 
-      expect(page).to have_content("#{shelter_1.name}")
-      expect(page).to have_content("#{shelter_1.address}")
-      expect(page).to have_content("#{shelter_1.city}")
-      expect(page).to have_content("#{shelter_1.state}")
-      expect(page).to have_content("#{shelter_1.zip}")
+      expect(Shelter.last.address).to eq('456 South St')
+      expect(Shelter.last.city).to eq('Springfield')
+      expect(Shelter.last.state).to eq('IL')
+      expect(Shelter.last.zip).to eq(85308)
     end
   end
 end
