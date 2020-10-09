8c8afd53acf2b4a149b67efb03b0659535443f48
Sean Steel <seanrsteel@gmail.com>
2020-10-09 09:11:15 -0600

    Create shelter_pet view

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -8,21 +8,21 @@ class PetsController < ApplicationController
   end
 
   def show
-    @pets = Pet.find(params[:id])
+    @pet = Pet.find(params[:id])
   end
 
   def create
-    @shelter = Shelter.find(params[:id])
-    binding.pry
-    shelter.pets.create({
+    shelter = Shelter.find(params[:id])
+    pet = shelter.pets.create({
       image: params[:pet][:image],
       name: params[:pet][:name],
       description: params[:pet][:description],
       approximate_age: params[:pet][:approximate_age],
       sex: params[:pet][:sex],
       adoption_status: 'Adoptable',
-      shelter_id: params[:shelter][:id]
+      shelter_id: params[:id]
     })
+    binding.pry
     pet.save
 
     redirect_to "/shelters/#{shelter.id}/pets"
--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,16 @@ Rails.application.routes.draw do
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   post '/shelters', to: 'shelters#create'
+  post '/shelters/:id/pets', to: 'pets#create'
   get '/shelters/:id', to: 'shelters#show'
-  get '/pets/:id', to: 'pets#show'
   get '/shelters/:id/pets', to: 'shelters#shelter_pet'
   get '/shelters/:id/pets/new', to: 'pets#new'
-  post '/shelters/:id/pets', to: 'pets#create'
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
 
   get '/pets', to: 'pets#index'
+  get '/pets/:id', to: 'pets#show'
 
 end
 
